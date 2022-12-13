e0e36604111553e0819119ea3ae98b288755adc9
danieltdocarmo <danielt.docarmo@gmail.com>
2022-12-13 15:34:05 -0300

    Create get and post for notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+
+@Controller('notification')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  get() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+
+    const { recipientId, content, category} = body;
+    await this.prismaService.notification.create({
+      data:{
+        id: randomUUID(),
+        recipientId: recipientId,
+        content: content,
+        category: category
+      }
+    })
   }
 }
