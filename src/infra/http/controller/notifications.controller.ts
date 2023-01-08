d0486f2d0f68eddf9cf83e2c877e4276daa1d791
Daniel do Carmo <danieldocarmo@Daniels-iMac-Pro.local>
2023-01-07 22:36:53 -0300

    Adiciona services aos seus controllers

--- src/infra/http/controller/notifications.controller.ts
+++ src/infra/http/controller/notifications.controller.ts
@@ -1,26 +1,53 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { randomUUID } from 'crypto';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/dto-create-notification';
-import { PrismaService } from '../../database/prisma/prisma.service';
-import { NotificationRepository } from 'src/application/repositories/NotificationRepository';
 import { SendNotification } from 'src/application/use-cases/send-notification';
-
+import { CancelNotification } from 'src/application/use-cases/cancel-notification';
+import { CoutNotificationByRecipientId } from 'src/application/use-cases/cout-notifications-by-recipient';
+import { GetManyNotificationsByRecipientId } from 'src/application/use-cases/get-notifications-by-recipient-id';
+import { ReadNotification } from 'src/application/use-cases/read-notification';
+import { UnreadNotification } from 'src/application/use-cases/unread-notification';
+import { Notification } from 'src/application/entities/Notification';
+import 
 
 @Controller('notification')
 export class NotificationsController {
-  constructor(private readonly sendNotification: SendNotification) {}
+  constructor(private readonly sendNotificationService: SendNotification,
+    private readonly cancelNotificationService: CancelNotification,
+    private readonly coutNotificationByRecipientId: CoutNotificationByRecipientId,
+    private readonly getManyNotificationsByRecipientId: GetManyNotificationsByRecipientId,
+    private readonly readNotificationService: ReadNotification,
+    private readonly unreadNotificationService: UnreadNotification){}
 
-  @Get()
-  async get() {
-  
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotificationService.execute({ notificationId: id });
+  }
+
+  @Get('count/from/:recipientId')
+  async countByRecipient(@Param('recipientId') recipientId: string):Promise<Number> {
+    const {value} = await this.coutNotificationByRecipientId.execute({ recipientId })
+    return value;
+  }
+
+  @Get('get/from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string):Promise<Notification[]> {
+    const {notifications} = await this.getManyNotificationsByRecipientId.execute({ recipientId })
+    return notifications;
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotificationService.execute({ notificationId: id })
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotificationService.execute({ notificationId: id })
   }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
-
-    const { recipientId, content, category} = body;
-    
-    await this.sendNotification.execute({recipientId, content, category});
-   
+    const { recipientId, content, category } = body;
+    await this.sendNotificationService.execute({ recipientId, content, category });
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,21 @@
 import { Module } from "@nestjs/common";
+import { CancelNotification } from "src/application/use-cases/cancel-notification";
+import { CoutNotificationByRecipientId } from "src/application/use-cases/cout-notifications-by-recipient";
+import { GetManyNotificationsByRecipientId } from "src/application/use-cases/get-notifications-by-recipient-id";
+import { ReadNotification } from "src/application/use-cases/read-notification";
 import { SendNotification } from "src/application/use-cases/send-notification";
+import { UnreadNotification } from "src/application/use-cases/unread-notification";
 import { DatabaseModule } from "../database/database.module";
 import { NotificationsController } from "./controller/notifications.controller";
 
 @Module({
-    imports:[DatabaseModule],
-    controllers:[NotificationsController],
-    providers:[SendNotification]
+    imports: [DatabaseModule],
+    controllers: [NotificationsController],
+    providers: [SendNotification,
+        CancelNotification,
+        ReadNotification,
+        UnreadNotification,
+        GetManyNotificationsByRecipientId,
+        CoutNotificationByRecipientId]
 })
-export class HttpModule{}
+export class HttpModule { }
